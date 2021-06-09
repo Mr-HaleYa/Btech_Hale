3d7dd2e6419e03549d2a208d1a52a1249943db85
Koby Hale <42821995+Mr-HaleYa@users.noreply.github.com>
2021-06-09 16:46:18 -0600

    Update Micro_1_robot.ino

--- Micro_1_robot/Micro_1_robot.ino
+++ Micro_1_robot/Micro_1_robot.ino
@@ -6,6 +6,9 @@
 #define MC3A 9
 #define MC4A 8
 
+#define IRL 0
+#define IRR 1
+
 unsigned long myTime = millis();
 
 void setup() {
@@ -20,19 +23,35 @@ void setup() {
 }
 
 void loop() {
-//  track();
-test();
+  //  track();
+  autoNav();
+  //test();
 }
 
-void test() {
-  forward   (3 * 1000);
+void autoNav() {
+  //  int dist = analogRead( IRL );
+
+  forward1   (100 * 1000);
+
 }
 
 void track() {
-  forward   (3 * 1000);
-  left      (2 * 100);
+  forward   (1.5 * 1000);
+  left      (1.5 * 100);
   forward   (1 * 1000);
-  left      (2 * 100);
+  left      (1 * 100);
+  forward   (0.3 * 1000);
+
+
+  left      (7 * 100);
+
+
+  forward   (0.6 * 1000);
+
+
+  left      (7 * 100);
+
+
   forward   (3 * 1000);
   right     (2 * 100);
   forward   (3 * 100);
@@ -49,14 +68,38 @@ void track() {
   }
 }
 
+void forward1 (int x) {
+  int L = 0;
+  int R = 0;
+  int MSL = 0;
+  int MSR = 0;
+  Serial.println("\nForward");
+  myTime = millis();
+  Serial.print("Start - "); Serial.println(myTime);
+  while (myTime + x > millis()) {
+    L = analogRead( IRL );
+    R = analogRead( IRR );
+    MSL = map( L, 800 , 400 , 0, 255 );
+    Serial.println(MSL);
+    MSR = map( R, 800 , 400 , 0, 255 );
+    analogWrite (EN1, MSR);   // LEFT MOTOR
+    digitalWrite(MC1A, LOW);
+    digitalWrite (MC2A, HIGH);
+    analogWrite (EN2, MSL);   // RIGHT MOTOR
+    digitalWrite(MC3A, LOW);
+    digitalWrite (MC4A, HIGH);
+  }
+  Serial.print("End - "); Serial.println(millis());
+}
+
 void forward (int x) {
   Serial.println("\nForward");
   myTime = millis();
   Serial.print("Start - "); Serial.println(myTime);
-  analogWrite (EN1, 255);
+  analogWrite (EN1, 250);
   digitalWrite(MC1A, LOW);
   digitalWrite (MC2A, HIGH);
-  analogWrite (EN2, 60);
+  analogWrite (EN2, 255);
   digitalWrite(MC3A, LOW);
   digitalWrite (MC4A, HIGH);
   while (myTime + x > millis()) {
